02507c7ba053540519d3a5fcd26b82eddf9ec8a7
Nagy Enikő <nagye99@gmail.com>
2021-11-07 18:59:20 +0100

    A futáshoz szükséges metódusok meghívása

--- ticket-service/src/main/java/com/epam/training/ticketservice/Application.java
+++ ticket-service/src/main/java/com/epam/training/ticketservice/Application.java
@@ -1,6 +1,15 @@
 package com.epam.training.ticketservice;
 
+import com.epam.training.ticketservice.ui.interpreter.CommandLineInterpreter;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.io.IOException;
+
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.epam.training.ticketservice");
+        CommandLineInterpreter commandLineInterpreter = context.getBean(CommandLineInterpreter.class);
+        commandLineInterpreter.handleUserInputs();
     }
 }
