ae59a9c9d048fd732df810cc1d27eb6202145326
nagye99 <nagye99@gmail.com>
2021-11-13 01:53:30 +0100

    Spring shell beépítése

--- ticket-service/src/main/java/com/epam/training/ticketservice/Application.java
+++ ticket-service/src/main/java/com/epam/training/ticketservice/Application.java
@@ -1,15 +1,16 @@
 package com.epam.training.ticketservice;
 
 import com.epam.training.ticketservice.ui.interpreter.CommandLineInterpreter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.io.IOException;
 
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) throws IOException {
-        ApplicationContext context = new AnnotationConfigApplicationContext("com.epam.training.ticketservice");
-        CommandLineInterpreter commandLineInterpreter = context.getBean(CommandLineInterpreter.class);
-        commandLineInterpreter.handleUserInputs();
+        SpringApplication.run(Application.class, args);
     }
 }
