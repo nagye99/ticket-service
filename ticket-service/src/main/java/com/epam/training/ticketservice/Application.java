d6e68f625763f7cfaa7eb219adeb9e8c8c8870a1
nagye99 <nagye99@gmail.com>
2021-11-14 11:12:23 +0100

    Main osztály létrehozása spring application segítségével

--- ticket-service/src/main/java/com/epam/training/ticketservice/Application.java
+++ ticket-service/src/main/java/com/epam/training/ticketservice/Application.java
@@ -1,16 +1,11 @@
 package com.epam.training.ticketservice;
 
-import com.epam.training.ticketservice.ui.interpreter.CommandLineInterpreter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import java.io.IOException;
 
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
         SpringApplication.run(Application.class, args);
     }
 }
