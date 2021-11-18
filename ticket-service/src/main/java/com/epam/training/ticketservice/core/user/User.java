93918408714f7030ab2273d40c0b261bf3464386
nagye99 <nagye99@gmail.com>
2021-11-18 23:10:33 +0100

    Az alapvető User tulajdonságok létrehozva

---
+++ ticket-service/src/main/java/com/epam/training/ticketservice/core/user/User.java
@@ -0,0 +1,13 @@
+package com.epam.training.ticketservice.core.user;
+
+public class User {
+    private final String username;
+    private final String password;
+    private final UserType userType;
+
+    public User(String username, String password, UserType userType) {
+        this.username = username;
+        this.password = password;
+        this.userType = userType;
+    }
+}
---
+++ ticket-service/src/main/java/com/epam/training/ticketservice/core/user/UserType.java
@@ -0,0 +1,6 @@
+package com.epam.training.ticketservice.core.user;
+
+public enum UserType {
+    ADMIN,
+    ORDINARY;
+}
